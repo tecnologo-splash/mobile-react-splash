e816acbbd0d4a4391611348a94fe20eb0a10e65f
Gastón López <gaston.lopez24299@gmail.com>
2021-06-08 23:30:11 -0300

    adelanto de listar publicaciones

--- src/pantallas/Muro.js
+++ src/pantallas/Muro.js
@@ -1,21 +1,24 @@
 import React, {useContext, useEffect, useState} from 'react';
 import { View, Text } from 'react-native';
 import {Context as ListarUsuariosContext} from '../context/ListarUsuariosContext';
+import {Context as PublicacionContext} from '../context/PublicacionContext';
 
 import NavBar from '../componentes/muro/NavBar';
 import ListadoUsuarios from '../componentes/listado-usuarios/ListadoUsuarios';
 import ListadoSugeridos from '../componentes/listado-sugeridos/ListadoSugeridos';
+import ListadoPublicaciones from '../componentes/publicaciones/ListadoPublicaciones';
 
 const Muro = () => {
 
   const [page, setPage] = useState(0);
   const {state:{filtro,buscar,usuarios, sugeridos},listarUsuariosParaSeguir, listarUsuariosSugeridos} = useContext(ListarUsuariosContext);
-
+  const {state:{publicaciones}, listarPublicacionesMuro} = useContext(PublicacionContext);
   useEffect(()=>{
     listarUsuarios(0);
     listarSugeridos(0);
+    listarPublicacionesMuro();
     setPage(0);
-  },[buscar, filtro])
+  },[buscar, filtro]);
 
   const listarUsuarios = (pagina)=>{
     listarUsuariosParaSeguir({filtro, valor:buscar, page: pagina});
@@ -36,6 +39,7 @@ const Muro = () => {
         <View>
           <Text>Muro</Text>
           <ListadoSugeridos sugeridos={sugeridos} onEnd={()=>listarSugeridos(page)}/>
+          <ListadoPublicaciones publicaciones = {publicaciones} />
         </View>
       }
     </View>
